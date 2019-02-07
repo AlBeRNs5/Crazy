7d196f4f31a5769bafd32dbb33cd4b6175dcaf9b
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-02-07 16:48:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '!'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`AlBeRNs Crazy `,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(` Crazy ! `,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -1185,5 +1185,48 @@ const pubg = [
  }
 });
 
+client.on("message",async message => {
+if(message.content === '*shop'){//الامر
+let staff = message.guild.member(message.author).roles.find('name' ,"seller");
+      if(!staff) return message.reply(`**Only Sellers | :x:**`)
+var shopc = message.guild.channels.find("name","sales")
+  if(!shopc) return message.reply("لا اجد الروم المخصص للبيع")
+    let shop = '';
+      let fillter = m => m.author.id === message.author.id
+      
+     
+
+      await message.channel.send("اكتب الاشياء الذي سوف تبيعها").then(e => {
+           message.channel.awaitMessages(fillter, { time: 60000, max: 1                                    
+})
+     .then(co => {
+       shop = co.first().content;
+        co.first().delete();
+     
+let desc = '';
+        
+e.edit("اكتب الدفع عند مين؟").then(e => {
+  message.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+
+     .then(co => {
+       desc = co.first().content;
+        co.first().delete();
+e.edit("Done").then(e => {
+  shopc.send(`@everyone 
+${message.guild.name}:tm: Shop :arrow_down:
+======================
+${shop}
+=================
+**الدفع عند:** **${desc}**
+**تم الارسال بواسطة:** ${message.author}
+@everyone `)
+  })
+})
+  })
+})
+  })
+           
+     
+	
 
 client.login(process.env.BOT_TOKEN);
