f54a34d379e54b4fc643d88579499026e4a7585e
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-03-09 23:22:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1803,5 +1803,60 @@ ${shop}
 }
 });
 
+ client.on("message", (message) => {
+    
+                        if (message.content.startsWith(prefix + "new")) {
+        const reason = message.content.split(" ").slice(1).join(" ");
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(` \`Support Team\` يجب عليك عمل رتبه بأسم واعطائها للبوت  والشخص الذي يستحكم بالتيكيت .`);
+        if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Team");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`تم أنشاء التذكره , #${c.name}.`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(` ${message.author.username}!`, ` *** شكرًا لك على التواصل مع فريق الدعم! سنرد عليك في أقرب وقت ممكن.. *** `)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error); 
+    }
+
+                        if (message.content.startsWith(prefix + "close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج روم التذكره.`);
+
+        message.channel.send(` ***هل أنت متأكد من إغلآق التذكرة ؟, Type /close to close the ticket., لديك 10 ثوآني للتأكيد .***`)
+            .then((m) => {
+                message.channel.awaitMessages(response => response.content === '/close', {
+                        max: 1,
+                        time: 10000,
+                        errors: ['time'],
+                    })
+                    .then((collected) => {
+                        message.channel.delete();
+                    })
+                    .catch(() => {
+                        m.edit('أنتهى الوقت لن يتم أغلاق لتذكره').then(m2 => {
+                            m2.delete();
+                        }, 3000);
+                    });
+            });
+    }
+
+});
+
 
 client.login(process.env.BOT_TOKEN);
